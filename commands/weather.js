c0da29a28eb7f9fa791be3b0460a022d512e3535
cherry-pie <aleksandr.mashnov@paydek.com>
2025-12-24 01:21:44 +0300

    weather fix

--- commands/weather.js
+++ commands/weather.js
@@ -13,7 +13,7 @@ const moduleParam = {
     serviceUrl: `https://api.open-meteo.com/v1/forecast?latitude=${homeLatitude}&longitude=${homeLongitude}&daily=temperature_2m_min,temperature_2m_max,precipitation_probability_max&timezone=${encodeURIComponent(homeTimeZone)}`,
 }
 
-const getWeatherMessage = async (bot, { needRemove, needAnswer, needButtons }) => {
+const getWeatherMessage = async (ctx, { needRemove, needAnswer, needButtons }) => {
     const serviceResponse = await fetch(moduleParam.serviceUrl);
     const serviceData = await serviceResponse.json();
 
@@ -27,16 +27,16 @@ const getWeatherMessage = async (bot, { needRemove, needAnswer, needButtons }) =
         `\n Максимальная температура: ${maxTemperature}°C` +
         `\n Вероятность осадков: до ${precipitation}%`
 
-    const isPrivateChat = bot.chat?.type === 'private';
+    const isPrivateChat = ctx.chat?.type === 'private';
 
-    await sendMessage(bot, {
+    await sendMessage(ctx, {
         accountId: isPrivateChat ? undefined : homeChatId,
         text: messageText,
         buttons: isPrivateChat ? homeOption : needButtons ? closeOption : {},
     });
 
     if (needRemove) {
-        await removeMessage(bot);
+        await removeMessage(ctx);
     }
 
     if (needAnswer) {
