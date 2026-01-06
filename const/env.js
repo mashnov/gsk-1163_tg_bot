82438cb6e826c8bd7c719bd2c61f28da42707f81
cherry-pie <aleksandr.mashnov@paydek.com>
2026-01-06 23:50:20 +0300

    add logger

--- const/env.js
+++ const/env.js
@@ -4,6 +4,7 @@ const botToken = process.env.BOT_TOKEN;
 const botUsername = process.env.BOT_USERNAME;
 const superUserId = Number(process.env.SUPER_USER_ID);
 const homeChatId = Number(process.env.HOME_CHAT_ID);
+const logMaxAgeDays = Number(process.env.LOG_MAX_AGE_DAYS);
 const homeTimeZone = process.env.HOME_TZ;
 const homeLatitude = process.env.HOME_WEATHER_LAT;
 const homeLongitude = process.env.HOME_WEATHER_LON;
@@ -22,6 +23,7 @@ module.exports = {
     botUsername,
     superUserId,
     homeChatId,
+    logMaxAgeDays,
     homeTimeZone,
     homeLatitude,
     homeLongitude,
--- helpers/logger.js
+++ helpers/logger.js
@@ -1,5 +1,24 @@
+const fs = require('fs');
+
+const { logMaxAgeDays } = require('../const/env');
+
+const FILE_PATH = './state/messages.txt';
+const LOG_MAX_AGE = logMaxAgeDays * 24 * 60 * 60 * 1000;
+
 const messageLogger = ({ from, to, text }) => {
-    console.log('logger', { from, to, text });
+    if (!fs.existsSync(FILE_PATH)) {
+        fs.writeFileSync(FILE_PATH, '');
+    }
+
+    const currentDate = new Date();
+    const record = { from, to, text: String(text ?? '').trim(), date: currentDate.toISOString() };
+    const content = fs.readFileSync(FILE_PATH, 'utf8') || '[]';
+    const records = [...JSON.parse(content), record];
+    const filteredRecords = records.filter((record) => {
+        const time = new Date(record.date).getTime();
+        return currentDate - time <= LOG_MAX_AGE;
+    });
+    fs.writeFileSync(FILE_PATH, JSON.stringify(filteredRecords));
 };
 
 module.exports = {
